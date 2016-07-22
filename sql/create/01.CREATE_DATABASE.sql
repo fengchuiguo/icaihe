a569d58eb69352ee9d928a3414b18d61acd4b504
liuhaiyuan <liuhy@shoukechou.com>
2016-07-22 15:51:09 +0800

    update

--- sql/create/01.CREATE_DATABASE.sql
+++ sql/create/01.CREATE_DATABASE.sql
@@ -1,9 +1,9 @@
 --数据库初始化脚本
 
 --创建数据库
-CREATE DATABASE icaihe;
-ALTER DATABASE icaihe DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
--- CREATE DATABASE icaihe DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+-- CREATE DATABASE icaihe;
+-- ALTER DATABASE icaihe DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+CREATE DATABASE icaihe DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
 
 --使用数据库
 use icaihe;
